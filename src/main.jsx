b6b37516d28b7f26f9405d3088d162ceed0cf12d
Justicilus <kaptikacticaganaras3@gmail.com>
2026-09-05 00:11:52 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AnimaApp from "./App.jsx";
+import Auth from "./Auth.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AnimaApp />
+    <Auth>
+      {(session, cikisYap) => <AnimaApp session={session} cikisYap={cikisYap} />}
+    </Auth>
   </React.StrictMode>
 );
