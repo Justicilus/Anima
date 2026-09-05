84c1ceb9fd8ce80be10fd7d9e2a982e4f65a516b
Justicilus <kaptikacticaganaras3@gmail.com>
2026-09-05 15:43:07 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import AnimaApp from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GlennaApp from "./App.jsx";
 import Auth from "./Auth.jsx";
+import Onboarding, { onboardingGorulduMu } from "./Onboarding.jsx";
+import PrivacyPolicy from "./PrivacyPolicy.jsx";
+import TermsOfService from "./TermsOfService.jsx";
+import NotFound from "./NotFound.jsx";
 import "./index.css";
+
+function KokEkran() {
+  const [gosterOnboarding, setGosterOnboarding] = useState(!onboardingGorulduMu());
+
+  if (gosterOnboarding) {
+    return <Onboarding onBitir={() => setGosterOnboarding(false)} />;
+  }
+
+  return (
+    <Auth>
+      {(session, cikisYap) => <GlennaApp session={session} cikisYap={cikisYap} />}
+    </Auth>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Auth>
-      {(session, cikisYap) => <AnimaApp session={session} cikisYap={cikisYap} />}
-    </Auth>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<KokEkran />} />
+        <Route path="/gizlilik" element={<PrivacyPolicy />} />
+        <Route path="/kullanim-sartlari" element={<TermsOfService />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
